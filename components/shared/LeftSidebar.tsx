fbefc079a7cf3f3b424a2bc4d75cd5cab88de411
Vinayak <vinayakcpa@gmail.com>
2023-08-13 19:20:19 -0400

    added constants

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,9 @@
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-cvol gap-6 px-6"></div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
