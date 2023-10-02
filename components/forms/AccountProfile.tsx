540561adbad2f6aa7251a90b854d88bc6eb44069
Vinayak <vinayakcpa@gmail.com>
2023-10-02 14:01:42 -0400

    edited AccountProfile and user validations

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useForm } from 'react-hook-form';
-import { Form
- } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
 
 interface Props {
   user: {
@@ -17,15 +18,16 @@ interface Props {
 }
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
-  
   const form = useForm({
-    resolver:  
-})
-  return (
-    <Form>
-
-    </Form>
-  )
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+  return <Form></Form>;
 };
 
 export default AccountProfile;
